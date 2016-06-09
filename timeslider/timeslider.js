cbd9924837463d8c56f705d40727b0fcdb3ecd83
Chris Brown <cbrown@fico.com>
2016-06-09 08:49:13 +0100

    Workaround for an error in PhantomJS caused by a local variable having the same name as a getter

--- timeslider/timeslider.js
+++ timeslider/timeslider.js
@@ -725,7 +725,7 @@ define(function(require, exports, module) {
              * Sets the loading state of the timeslider to match its active document's revisions fetching
              * @property {Boolean} loading
              */
-            set loading(loading) { setLoading(loading); },
+            set loading(ldng) { setLoading(ldng); },
             /**
              * Gets the timeslider's active document or null, if not any
              * @property {Document} activeDocument
